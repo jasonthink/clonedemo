bb1caa12bbfe7f2db5af3eba301c770eeeafec80
Jason Erickson <jaericks@us.ibm.com>
2020-11-17 14:49:40 -0700

    more logging

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func rootHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println("hello called")
 	fmt.Fprintf(w, "<html><head><title>Test Clone and Reinstate</title><body><h1>Demo</h1><p><script type=\"text/javascript\">document.write(location.href);</script></p></body></html>")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
+	log.Println("/headers requested")
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
@@ -18,7 +21,11 @@ func headers(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	log.Println("Starting")
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/hello", rootHandler)
 	http.HandleFunc("/headers", headers)
-	http.ListenAndServe(":80", nil)
+	log.Println("Listening on 3000")
+	http.ListenAndServe(":3000", nil)
+	log.Println("Exiting")
 }
